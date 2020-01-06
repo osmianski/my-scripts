19afe33e877b45852aff2888ee4c134ba8c6c54b
Vladislav Osmianskij <vlaosm@gmail.com>
2020-01-06 22:20:47 +0200

    fix: `deploy osmdocs.com` adding default queue

--- src/Deploy/Commands/OsmdocsCom.php
+++ src/Deploy/Commands/OsmdocsCom.php
@@ -58,7 +58,7 @@ class OsmdocsCom extends Command
             $this->shell->cd($this->path, function() {
                 $this->shell->run("git reset --hard HEAD");
                 $this->shell->run("git pull origin v1");
-                $this->shell->run("composer update --no-scripts");
+                $this->shell->run("composer install --no-scripts");
                 $this->shell->run("php run migrations");
                 $this->shell->run("php run config:npm");
                 $this->shell->run("npm install");
@@ -69,7 +69,7 @@ class OsmdocsCom extends Command
         $this->shell->cd($this->root_path, function() {
             $this->shell->run("git reset --hard HEAD");
             $this->shell->run("git pull origin v1");
-            $this->shell->run("composer update --no-scripts");
+            $this->shell->run("composer install --no-scripts");
             $this->shell->run("php fresh");
         });
 
